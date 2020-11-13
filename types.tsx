c1bfcf63cc10a2199c20c85bdccc05e4ea6d46ba
kaahulkkonen <kaa.hulkkonen@gmail.com>
2020-11-13 19:16:38 +0200

    Refactor types to one file, allow for multiple colors

--- types.tsx
+++ types.tsx
@@ -16,8 +16,20 @@ export type FavoritesParamList = {
   FavoritesScreen: undefined;
 };
 
+export type UnsplashItem = {
+  id: number;
+  src: string;
+  colors: Color[];
+}
+
 export type Color = {
   r: number,
   g: number,
   b: number
+}
+
+const asd: UnsplashItem = {
+  id: 1,
+  src: '',
+  colors: [{r: 1, g: 2, b: 3}, {r: 1, g: 2, b: 3}]
 }
