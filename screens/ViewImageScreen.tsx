8b0ae34722297758d271bdc0a6cf7c5d6400df7d
kaahulkkonen <kaa.hulkkonen@gmail.com>
2020-11-14 20:30:00 +0200

    DIY Pinch to zoom

--- screens/ViewImageScreen.tsx
+++ screens/ViewImageScreen.tsx
@@ -1,23 +1,33 @@
 import * as React from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, Animated } from 'react-native';
 import { View } from '../components/Themed';
 import { getImageSrc } from '../utils/images';
 import { ImageQualityType, UnsplashItem } from '../state/images/types';
 import { Ionicons } from '@expo/vector-icons';
+import { PinchGestureHandler, PanGestureHandler } from "react-native-gesture-handler"
 
 export default function ViewImageScreen(params: any) {
   const { item } = params.route.params;
 
+  const scale = React.useRef(new Animated.Value(1)).current;
+  const translationX = React.useRef(new Animated.Value(0)).current;
+  const translationY = React.useRef(new Animated.Value(0)).current;
+
+  const handlePinch = Animated.event([{ nativeEvent: { scale } }], { useNativeDriver: false });
+  const handlePan = Animated.event([{ nativeEvent: { translationX, translationY } }], { listener: e => console.log(e.nativeEvent), useNativeDriver: false });
+
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={{ uri: getImageSrc(item, ImageQualityType.HIGH_QUALITY)}}
-        style={styles.image}
-        resizeMode={'contain'}>
-        <View style={{backgroundColor: 'transparent', zIndex: 999}} onTouchStart={() => {params.navigation.goBack()}}>
-          <Ionicons style={{marginRight: 10}} name='ios-close' color={'#ffffff'} size={42} />
-        </View>
-      </ImageBackground>
+      <View style={{ backgroundColor: 'transparent', zIndex: 999 }} onTouchStart={() => { params.navigation.goBack() }}>
+        <Ionicons style={{ marginRight: 10 }} name='ios-close' color={'#ffffff'} size={42} />
+      </View>
+
+          <Animated.Image
+            source={{ uri: getImageSrc(item, ImageQualityType.HIGH_QUALITY) }}
+            style={[styles.image, { transform: [{ scale }, {translateX: translationX}, {translateY: translationY }]}]}
+            resizeMode={'contain'}
+          >
+          </Animated.Image>
     </View>
   );
 }
